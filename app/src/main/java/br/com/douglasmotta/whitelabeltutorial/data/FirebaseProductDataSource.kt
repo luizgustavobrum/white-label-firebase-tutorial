2ac0dc4dd73b44480376acc3a93e7324fe381b2e
luizgustavobrum <luizgustavo.brum@gmail.com>
2021-08-29 12:40:19 -0300

    Implementando a classe FirebaseProductDataSource

---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -0,0 +1,55 @@
+package br.com.douglasmotta.whitelabeltutorial.data
+
+import android.net.Uri
+import br.com.douglasmotta.whitelabeltutorial.BuildConfig
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
+import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
+import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class FirebaseProductDataSource(
+    private val firebaseFirestore: FirebaseFirestore,
+    private val firebaseStore: FirebaseStorage
+) : ProductDataSource {
+
+    //data/car/products/timestamp/productA
+    //data/bike/products/timestamp/productB
+
+    private val documentReference = firebaseFirestore
+        .document("$COLLECTION_ROOT/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/")
+
+    private val storeReference = firebaseStore.reference
+
+    override suspend fun getProducts(): List<Product> {
+        return suspendCoroutine { continuation ->
+            val productReference = documentReference.collection(COLLECTION_PRODUCTS)
+            productReference.get().addOnSuccessListener { documents ->
+                val products = mutableListOf<Product>()
+                documents.forEach { document ->
+                    document.toObject(Product::class.java).run {
+                        products.add(this)
+                    }
+                }
+
+                continuation.resumeWith(Result.success(products))
+            }
+
+            productReference.get().addOnFailureListener { execption ->
+                continuation.resumeWithException(execption)
+
+            }
+        }
+    }
+
+    override suspend fun uploadProductImage(imageUri: Uri): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createProduct(product: Product): Product {
+        TODO("Not yet implemented")
+    }
+
+}
