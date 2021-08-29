cefa60d5ebbbba9af40babb651f4ffa346d68040
luizgustavobrum <luizgustavo.brum@gmail.com>
2021-08-29 20:47:48 -0300

    Implementação dos métodos uploadProductImage e createProduct do repositório FirebaseProductDataSource

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -5,8 +5,11 @@ import br.com.douglasmotta.whitelabeltutorial.BuildConfig
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
@@ -45,11 +48,39 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childReference = storeReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+
+            childReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapshot ->
+                    taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        val path = uri.toString()
+                        continuation.resumeWith(Result.success(path))
+                    }
+
+                }.addOnFailureListener { execption ->
+                    continuation.resumeWithException(execption)
+                }
+
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            documentReference
+                .collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resume(product)
+
+                }.addOnFailureListener { execption ->
+                    continuation.resumeWithException(execption)
+                }
+        }
     }
 
 }
